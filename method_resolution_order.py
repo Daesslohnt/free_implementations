7dd610e5880496a275d36bbf9096af878e87f6e4
Daesslohnt <fridrich1900@gmail.com>
2022-07-04 06:41:35 +0200

    mro

---
+++ method_resolution_order.py
@@ -0,0 +1,29 @@
+class Root(object):
+    def f(self):
+        print("Root.f", self)
+
+class A(Root):
+    def f(self):
+        print("A.f", self)
+        super().f()
+
+class B(Root):
+    def f(self):
+        print("B.f", self)
+        super().f()
+
+class C(A, B): # mro( C, A, B, object)
+    def f(self):
+        print("C.f", self)
+        super().f()
+
+
+# class D(A, C):  error because C inherit from A
+
+
+if __name__ == '__main__':
+
+    print([cls.__name__ for cls in C.__mro__])
+
+    c = C()
+    c.f()
